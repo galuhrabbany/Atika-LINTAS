5d34049db6267bf0d3213a4f23fc058b51ea504b
AtqhPtr <putriistiatika13@gmail.com>
2025-03-12 08:48:35 +0700

    Menambahkan modal Point

---
+++ storage/framework/views/70c633679a292b4a0e0b3f240b52411c.php
@@ -0,0 +1,41 @@
+<?php $__env->startSection('content'); ?>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Kelas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Atika</td>
+                <td>23</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Fay</td>
+                <td>23</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Galuh</td>
+                <td>23</td>
+                <td>A</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Meiva</td>
+                <td>23</td>
+                <td>A</td>
+            </tr>
+        </tbody>
+    </table>
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layout.template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Atika Putri\Documents\KULIAH VIBES\SMSTR 4\PGWEBL\pgwebl2\resources\views/table.blade.php ENDPATH**/ ?>
